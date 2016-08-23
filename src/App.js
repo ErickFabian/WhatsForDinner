db03cf5a357b56763171de66ccc3661af7a3b9c6
ErickFabian <fabianerickalfonso@gmail.com>
2016-08-23 15:29:33 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import update from "react-addons-update";
+import Map from './components/map';
+import './App.css';
+
+class App extends Component {
+  handleMapClick = this.handleMapClick.bind(this);
+  handleMarkerRightclick = this.handleMarkerRightclick.bind(this);
+
+  state = {
+    markers: [{
+      position: {
+        lat: 20.6612363,
+        lng: -103.3298526,
+      },
+      key: `Home`,
+      defaultAnimation: 2,
+    }],
+  };
+
+  handleMapClick(event) {
+    let { markers } = this.state;
+    markers = update(markers, {
+      $push: [
+        {
+          position: event.latLng,
+          defaultAnimation: 2,
+          key:  Date.now(),
+        },
+      ],
+    });
+    this.setState({ markers });
+  }
+
+  handleMarkerRightclick(index, event) {
+    let { markers } = this.state;
+    markers = update(markers, {
+      $splice: [
+        [index, 1],
+      ],
+    });
+    this.setState({ markers });
+  }
+
+  render() {
+    return (
+      <Map
+        markers={this.state.markers}
+        onMapClick={this.handleMapClick}
+        onMarkerRightclick={this.handleMarkerRightclick}
+      />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/map.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+
+export default function Map (props) {
+  return (
+    <section style={{height: "100%"}}>
+      <GoogleMapLoader
+        containerElement={
+          <div
+            {...props.containerElementProps}
+            style={{
+              height: "100%",
+            }}
+          />
+        }
+        googleMapElement={
+          <GoogleMap
+            defaultZoom={15}
+            defaultCenter={{ lat: 20.6612363, lng: -103.3298526 }}
+            onClick={props.onMapClick}
+          >
+            {props.markers.map((marker, index) => {
+              return (
+                <Marker
+                  {...marker}
+                  onRightclick={() => props.onMarkerRightclick(index)} />
+              );
+            })}
+          </GoogleMap>
+        }
+      />
+    </section>
+  );
+}
