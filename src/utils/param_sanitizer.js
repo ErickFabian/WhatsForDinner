9841fd82fa334792aaf62738361e2b5c275c923d
ErickFabian <fabianerickalfonso@gmail.com>
2016-09-21 11:33:33 -0500

    Fix

--- src/utils/param_sanitizer.js
+++ src/utils/param_sanitizer.js
@@ -15,7 +15,7 @@ let ParamSanitizer = {
     });
 
     return newObject;
-  }
+  },
 
   underscored(str) {
     return str.replace(/([a-z\d])([A-Z]+)/g, '$1_$2').replace(/[-\s]+/g, '_').toLowerCase();
