b4aa746f4bb690e99c7735c7b7dfa42ab8c9a250
ErickFabian <fabianerickalfonso@gmail.com>
2016-09-21 11:05:38 -0500

    fix imports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.css';
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.css.map';
+import 'bootstrap-material-design/dist/css/ripples.css';
 
 ReactDOM.render(
   <App />,
